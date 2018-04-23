4534cd1cce6606767f45e9666d4a28eae4b91dfd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/ReportPreview.ascx.cs
@@ -0,0 +1,19 @@
+﻿#region #reportpreview
+using System;
+using DevExpress.XtraScheduler.Reporting;
+using DevExpress.Web.ASPxScheduler.Reporting;
+
+public partial class ReportPreview : System.Web.UI.UserControl {
+    ASPxSchedulerControlPrintAdapter var_controlAdapter;
+
+    public ASPxSchedulerControlPrintAdapter ControlAdapter { 
+        get { return var_controlAdapter; } 
+        set { var_controlAdapter = value; } }
+
+    protected void Page_Load(object sender, EventArgs e) {
+        XtraSchedulerReport1 rpt = new XtraSchedulerReport1();
+        rpt.SchedulerAdapter = ControlAdapter.SchedulerAdapter;
+        ReportViewer1.Report = rpt;
+    }
+}
+#endregion #reportpreview
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,45 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.Web.ASPxMenu
+Imports DevExpress.XtraScheduler
+
+Partial Public Class [Default]
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		DataHelper.SetupMappings(ASPxScheduler1)
+		DataHelper.ProvideRowInsertion(ASPxScheduler1, DataSource1.AppointmentDataSource)
+		DataSource1.AttachTo(ASPxScheduler1)
+
+		PrepareReportPreview(PreviewPanel)
+
+	End Sub
+
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxScheduler1.Start = DateTime.Today
+	End Sub
+	#Region "#preparereportpreview"
+	Private Sub PrepareReportPreview(ByVal cp As Control)
+	ASPxSchedulerControlPrintAdapter1.TimeInterval = New TimeInterval(DateTime.Today, DateTime.Today.AddDays(7))
+	ASPxSchedulerControlPrintAdapter1.WorkTime = New TimeOfDayInterval(TimeSpan.FromHours(9), TimeSpan.FromHours(18))
+
+	Dim reportPreview As ReportPreview = CType(Page.LoadControl("ReportPreview.ascx"), ReportPreview)
+	reportPreview.ControlAdapter = ASPxSchedulerControlPrintAdapter1
+
+	cp.Controls.Clear()
+	cp.Controls.Add(reportPreview)
+	End Sub
+	#End Region ' #preparereportpreview
+
+Protected Sub ASPxCallbackPanel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+		PreviewPanel.Visible = True
+End Sub
+End Class
