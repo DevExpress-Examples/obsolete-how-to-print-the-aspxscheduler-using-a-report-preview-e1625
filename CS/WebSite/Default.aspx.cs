2a51c49af6dc286d3d0a4f9948295b8327c8f308
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Web.ASPxScheduler;
+using DevExpress.Web.ASPxMenu;
+using DevExpress.XtraScheduler;
+
+public partial class Default : System.Web.UI.Page {
+	protected void Page_Load(object sender, EventArgs e) {
+		DataHelper.SetupMappings(ASPxScheduler1);
+		DataHelper.ProvideRowInsertion(ASPxScheduler1, DataSource1.AppointmentDataSource);
+		DataSource1.AttachTo(ASPxScheduler1);
+
+        PrepareReportPreview(PreviewPanel);
+
+	}
+    
+    protected void Page_Init(object sender, EventArgs e) {
+        ASPxScheduler1.Start = DateTime.Today;
+    }
+    #region #preparereportpreview
+    void PrepareReportPreview(Control cp) {
+    ASPxSchedulerControlPrintAdapter1.TimeInterval = 
+        new TimeInterval(DateTime.Today, DateTime.Today.AddDays(7));
+    ASPxSchedulerControlPrintAdapter1.WorkTime = 
+        new TimeOfDayInterval(TimeSpan.FromHours(9), TimeSpan.FromHours(18));
+
+    ReportPreview reportPreview = (ReportPreview)Page.LoadControl("ReportPreview.ascx");
+    reportPreview.ControlAdapter = ASPxSchedulerControlPrintAdapter1;
+
+    cp.Controls.Clear();
+    cp.Controls.Add(reportPreview);
+}
+    #endregion #preparereportpreview
+
+protected void ASPxCallbackPanel1_Callback(object sender, DevExpress.Web.ASPxClasses.CallbackEventArgsBase e) {
+        PreviewPanel.Visible = true;
+    }
+}
---
+++ VB/WebSite/ReportPreview.ascx.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#reportpreview"
+Imports System
+Imports DevExpress.XtraScheduler.Reporting
+Imports DevExpress.Web.ASPxScheduler.Reporting
+
+Partial Public Class ReportPreview
+	Inherits System.Web.UI.UserControl
+	Private var_controlAdapter As ASPxSchedulerControlPrintAdapter
+
+	Public Property ControlAdapter() As ASPxSchedulerControlPrintAdapter
+		Get
+			Return var_controlAdapter
+		End Get
+		Set(ByVal value As ASPxSchedulerControlPrintAdapter)
+			var_controlAdapter = value
+		End Set
+	End Property
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim rpt As New XtraSchedulerReport1()
+		rpt.SchedulerAdapter = ControlAdapter.SchedulerAdapter
+		ReportViewer1.Report = rpt
+	End Sub
+End Class
+#End Region ' #reportpreview
